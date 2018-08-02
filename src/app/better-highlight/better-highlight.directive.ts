ebe74c8dae53aad95f5868f2e9f0025e585e17e4
nakuyabridget <bnakuya7@gmail.com>
2018-08-02 15:30:38 +0300

    Host Binding

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 
 @Directive({
@@ -6,6 +6,12 @@ import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular
 })
 
 export class BetterHighlightDirective implements OnInit {
+
+    // @HostBinding decorator, to bind to the property style on top which this directive is sitting on top of.
+
+    @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+
+
     constructor (private elRef: ElementRef, private renderer: Renderer2) {
 
     }
@@ -16,10 +22,12 @@ export class BetterHighlightDirective implements OnInit {
 
     // @hostlistener decorater that works with mouseover methosd to turn our paragraph to blue whenever it is hovered over
     @HostListener('mouseenter') mouseover(eventData: Event) {
-        this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    }
+        this.backgroundColor = 'blue';
+             // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+ }
       //  @hostlistener decorator that works with mouseleave to turn our paragraph to transparent whenever the mosue leaves
     @HostListener('mouseleave') mouseleave(eventData: Event) {
-        this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+        this.backgroundColor = 'transparent';
+        // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
     }
 }
